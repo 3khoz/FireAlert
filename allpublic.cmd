6158469da5c9549478810d35b22473267326c296
3khoz <ignacio.diaz.h@gmail.com>
2018-03-15 16:21:03 -0300

    Automatizacion final
    Se crean dos batch para subir el html a google cloud y hacerlo publico

---
+++ allpublic.cmd
@@ -0,0 +1,3 @@
+@echo off
+gsutil acl ch -u AllUsers:R gs://my_sitio/AlertaIncendios_27022018.html
+exit
---
+++ upload.cmd
@@ -0,0 +1,3 @@
+@echo off
+gsutil cp C:\Users\Michelle1\Documents\GitHub\FireAlert\AlertaIncendios_27022018.html  gs://my_sitio
+exit
