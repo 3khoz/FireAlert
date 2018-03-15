248f0c1029fe57ccf51045eeb9c70827ff1dd1e4
3khoz <ignacio.diaz.h@gmail.com>
2018-03-15 11:25:28 -0300

    a1

--- Run.R
+++ Run.R
@@ -2,3 +2,5 @@ setwd("c:/Users/Michelle1/Documents/GitHub/FireAlert")
 library(rmarkdown)
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 render("AlertaIncendios_27022018.Rmd")
+
+
